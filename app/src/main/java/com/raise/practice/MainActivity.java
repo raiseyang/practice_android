b6752577798379ccaea67cb150d113b21d2f1e6b
raise-abup <y22222ly@163.com>
2019-08-05 21:04:44 +0800

    jsbridge

--- app/src/main/java/com/raise/practice/MainActivity.java
+++ app/src/main/java/com/raise/practice/MainActivity.java
@@ -1,11 +1,10 @@
 package com.raise.practice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.raise.practice.dagger2.DaggerActivity;
-import com.raise.practice.iotdialog.DialogActivity;
+import com.raise.practice.progressbtn.ProgressActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,10 +15,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void click_dagger2(View view) {
-        DaggerActivity.startActivity(this);
     }
 
     public void click_dialog(View view) {
-        DialogActivity.startActivity(this);
+    }
+
+    public void click_progress(View view) {
+        ProgressActivity.startActivity(this);
     }
 }
---
+++ app/src/main/java/com/raise/practice/jsbridge/JsActivity.kt
@@ -0,0 +1,31 @@
+package com.raise.practice.jsbridge
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.github.lzyzsd.jsbridge.BridgeHandler
+import com.github.lzyzsd.jsbridge.CallBackFunction
+import com.github.lzyzsd.jsbridge.DefaultHandler
+import com.raise.practice.R
+import kotlinx.android.synthetic.main.activity_js.*
+
+class JsActivity : AppCompatActivity() {
+
+    val TAG = "JsActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_js)
+
+        webView.setDefaultHandler(DefaultHandler())
+
+        webView.loadUrl("file:///android_asset/demo.html");
+
+        webView.registerHandler("submitFromWeb", object : BridgeHandler {
+            override fun handler(data: String?, function: CallBackFunction?) {
+                Log.i(TAG, "handler = submitFromWeb, data from web = " + data)
+                function?.onCallBack("submitFromWeb exe, response data 中文 from Java")
+            }
+        })
+    }
+}
