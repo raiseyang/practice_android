a9e87f3ac0dd8e8ad7f8e844069e4baf1f466321
yangdongsheng <yangdongsheng@wingtech.com>
2021-03-05 11:00:50 +0800

    更新主界面

--- app/src/main/java/com/raise/practice/MyApp.kt
+++ app/src/main/java/com/raise/practice/MyApp.kt
@@ -1,12 +1,19 @@
 package com.raise.practice
 
 import android.app.Application
+import android.content.Context
 import com.raise.weapon_base.LLog
+import kotlin.properties.Delegates
 
 class MyApp : Application() {
 
+    companion object {
+        var ctx: Context by Delegates.notNull()
+    }
+
     override fun onCreate() {
         super.onCreate()
+        ctx = this
         LLog.d("MyApp", "onCreate() this=$this")
 
     }
